74e200fc6cfd1d939fb55a39c19ab02da3a498c6
Brendan Cordova <cordovalegacy19@gmail.com>
2023-01-11 14:55:49 -0800

    Practice with Modularizing Folder Structure

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,6 +1,6 @@
 from flask_app import app
-from flask import render_template, redirect, request, session, flash
-from flask_app.models import user
+from flask import render_template, redirect, request
+from flask_app.models.user import User
 
 @app.route('/')
 def index():
