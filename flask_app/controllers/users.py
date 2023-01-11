cc5b0836b1de8759f72916760862a69c4ad644fb
Brendan Cordova <cordovalegacy19@gmail.com>
2023-01-11 14:46:51 -0800

    Modularized Version of Users CRUD

---
+++ flask_app/controllers/users.py
@@ -0,0 +1,52 @@
+from flask_app import app
+from flask import render_template, redirect, request, session, flash
+from flask_app.models import user
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/create_user', methods=['POST'])
+def create_user():
+    data = {
+        "fname": request.form['fname'],
+        "lname": request.form['lname'],
+        "eml": request.form['eml']
+    }
+    User.save(data)
+    return redirect('/display_user')
+
+
+@app.route('/display_user')
+def display_user():
+    users = User.get_all()
+    print(users)
+    return render_template('display_user.html', all_users=users)
+
+@app.route('/delete_user/<int:id>')
+def delete_user(id):
+    data={
+        'id':id
+    }
+    User.delete_user(data)
+    return redirect('/display_user')
+
+@app.route('/edit_user/<int:id>')
+def edit_user(id):
+    data={
+        'id':id
+    }
+    return render_template('edit_user.html', user=User.display_user_by_id(data))
+
+@app.route('/updated', methods=['POST'])
+def updated():
+    User.updated(request.form)
+    return redirect('/display_user')
+
+@app.route('/display_user_by_id/<int:id>')
+def display_user_by_id(id):
+    data={
+        'id':id
+    }
+    return render_template('display_user_by_id.html', user=User.display_user_by_id(data))
